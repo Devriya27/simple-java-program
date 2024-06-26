4701d6ba2664b690a6849b1b28bf210ac0ef9ed9
Devriya27 <deviarul12@gmail.com>
2024-06-26 22:23:44 +0530

    Add files via upload
    factorial

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+class Factorial
+{
+	static long i=1;
+	public static void main(String args[])
+	{
+		fact();
+	}
+
+	static void fact()
+	{
+		long out=1;
+		for(int j=1;j<=i;j++)
+		{
+			out=out*j;
+		}
+		System.out.println(out);
+		i++;
+		fact();
+	}
+
+}
